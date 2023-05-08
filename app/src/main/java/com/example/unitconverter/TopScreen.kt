9658a59f504573bfbb2a758509a921ac0f314b2f
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-08 15:40:55 +0200

    ResultBlock added

--- app/src/main/java/com/example/unitconverter/TopScreen.kt
+++ app/src/main/java/com/example/unitconverter/TopScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 
 @Composable
@@ -12,6 +14,7 @@ fun TopScreen(list: List<Conversion>) {
     val selectedConversion : MutableState<Conversion?> =  remember { mutableStateOf(null) }
 
     val inputText : MutableState<String> =  remember { mutableStateOf("") }
+    val typedValue = remember { mutableStateOf("0.0") }
 
     ConversionMenu(list = list){
        selectedConversion.value = it
@@ -19,8 +22,21 @@ fun TopScreen(list: List<Conversion>) {
 
     selectedConversion.value?.let {
         InputBlock(conversion = it, inputText = inputText){input->
-            Log.i("MYTAG","User typed $input")
+            typedValue.value = input
 
         }
     }
+    if (typedValue.value != "0.0") {
+       val input = typedValue.value.toDouble()
+        val multiply = selectedConversion.value!!.multiplyBy
+        val result = input*multiply
+
+        //rounding off the result to 4 decimal places
+        val df = DecimalFormat("#.####")
+        df.roundingMode = RoundingMode.DOWN
+        val roundedResult = df.format(result)
+
+
+    }
+
 }
