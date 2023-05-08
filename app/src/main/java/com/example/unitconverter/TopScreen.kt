40cb29c7b4940c61fc2bc8a2a8045db8aef42bde
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-08 14:07:20 +0200

    Input Blocks successfully implemented

--- app/src/main/java/com/example/unitconverter/TopScreen.kt
+++ app/src/main/java/com/example/unitconverter/TopScreen.kt
@@ -1,12 +1,23 @@
 package com.example.unitconverter
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 
 @Composable
 fun TopScreen(list: List<Conversion>) {
-    ConversionMenu(list = list){
+    val selectedConversion : MutableState<Conversion?> =  remember { mutableStateOf(null) }
 
+    val inputText : MutableState<String> =  remember { mutableStateOf("") }
+
+    ConversionMenu(list = list){
+       selectedConversion.value = it
+    }
+
+    selectedConversion.value?.let {
+        InputBlock(conversion = it, inputText = inputText)
     }
 
 }
