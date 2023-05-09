9b27ccc7510062546843ea993ed6d455631cebb1
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-09 17:46:58 +0200

    fix issue displaying text when rotating

--- app/src/main/java/com/example/unitconverter/compose/converter/ConversionMenu.kt
+++ app/src/main/java/com/example/unitconverter/compose/converter/ConversionMenu.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -23,7 +24,7 @@ import com.example.unitconverter.data.Conversion
 @Composable
 fun ConversionMenu(list: List<Conversion>, modifier: Modifier = Modifier, convert : (Conversion) -> Unit){
 
- var displayingText by remember{ mutableStateOf("Select the conversion type")}
+    var displayingText by rememberSaveable{ mutableStateOf("Select the conversion type")}
     var textFieldSize by remember{ mutableStateOf(Size.Zero) }//To assign the dropdown the same width as textField
     var expanded by remember { mutableStateOf(false) }
 
