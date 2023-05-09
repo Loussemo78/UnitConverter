54840e8ac399ab550b5f886e69595a6a8d949495
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-09 17:55:30 +0200

    fix issue multiple save data in database  when rotating and come back.

--- app/src/main/java/com/example/unitconverter/compose/converter/TopScreen.kt
+++ app/src/main/java/com/example/unitconverter/compose/converter/TopScreen.kt
@@ -1,9 +1,6 @@
 package com.example.unitconverter.compose.converter
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import com.example.unitconverter.data.Conversion
 import java.math.RoundingMode
 import java.text.DecimalFormat
@@ -18,6 +15,9 @@ fun TopScreen(
     save: (String,String) -> Unit
 ) {
 
+    var toSave by remember {
+        mutableStateOf(false)
+    }
 
     ConversionMenu(list = list){
        selectedConversion.value = it
@@ -27,7 +27,7 @@ fun TopScreen(
     selectedConversion.value?.let {
         InputBlock(conversion = it, inputText = inputText){input->
             typedValue.value = input
-
+            toSave = true
         }
     }
     if (typedValue.value != "0.0") {
@@ -42,7 +42,10 @@ fun TopScreen(
 
         val message1 = "${typedValue.value}  ${selectedConversion.value!!.convertFrom} is equal to"
         val message2 = "$roundedResult ${selectedConversion.value!!.convertTo}"
-        save(message1,message2)
+        if (toSave){
+            save(message1,message2)
+            toSave = false
+        }
         ResultBlock(message1 = message1, message2 = message2)
     }
 
